c3d80935c8c5bd4d548a507791132c1d0f35da8d
Nvhuma <nhlayisekobvuma7@gmail.com>
2024-03-05 20:49:54 +0000

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.>
+
+
+/**
+ * main - prints "Programming is like building  a multilingual puzzl,follow by a newline" 
+ *  return 0.
+  */
+int main(void)
+       	
+{ 
+	puts("/"programming is like  building a multilingual puzzle);
+	return(0);
+
+}
+  
