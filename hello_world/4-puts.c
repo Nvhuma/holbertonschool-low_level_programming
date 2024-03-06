08c432ea23b4ab57a8dd338c7dcf06f611253c61
Nvhuma <nhlayisekobvuma7@gmail.com>
2024-03-06 06:08:33 +0000

    file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,18 @@
-#include <stdio.>
-
-
+#include <stdio.h>
 /**
- * main - prints "Programming is like building  a multilingual puzzl,follow by a newline" 
- *  return 0.
-  */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-       	
-{ 
-	puts("/"programming is like  building a multilingual puzzle);
-	return(0);
-
-}
+	{
+		/* Using the function puts to print the desired message */
+		puts("Programming is like building a multilingual puzzle");
+		 /* Print a new line */
+		 puts("");
+		  /* End the program with the value 0 */
+		  return (0);
+	}
+gcc -Wall -Werror -Wextra -pedantic -std=c89 4-puts.c -o 4-puts
+./4-puts
   
