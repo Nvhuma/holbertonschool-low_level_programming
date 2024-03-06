88944ed2bad94f83e09ec3ff592dd47e33387a28
Nvhuma <nhlayisekobvuma7@gmail.com>
2024-03-06 06:45:27 +0000

    file

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main() {
 	printf("%s, but the outcome is a piece of art,\n", "Programming is like building a multilingual puzzle");
 	 return 0;
