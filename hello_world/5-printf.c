dfcc976cf6c50f47ed63ed4debe780ab23a8478a
Nvhuma <nhlayisekobvuma7@gmail.com>
2024-03-06 06:50:53 +0000

    file

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+
+
 /**
  * main - entry point of the program
  *
  * Description: Prints a message with proper grammar.
  * Return: Always 0 (Success)
  */
-int main() {
-	printf("%s, but the outcome is a piece of art,\n", "Programming is like building a multilingual puzzle");
+int main()
+{
+	printf("%s, but the outcome is a piece of art,\n",
+		       	"Programming is like building a multilingual puzzle");
 	 return 0;
-	 }
+ }
 
