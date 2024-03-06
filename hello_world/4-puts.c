e6a2edd8737d029b2bf280ad070a0900b6f15e0c
Nvhuma <nhlayisekobvuma7@gmail.com>
2024-03-06 06:14:07 +0000

    file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,13 +6,6 @@
  */
 int main(void)
 	{
-		/* Using the function puts to print the desired message */
-		puts("Programming is like building a multilingual puzzle");
-		 /* Print a new line */
-		 puts("");
-		  /* End the program with the value 0 */
-		  return (0);
+		puts("\"Programming is like building a multilungual puzze");
+		return (0);
 	}
-gcc -Wall -Werror -Wextra -pedantic -std=c89 4-puts.c -o 4-puts
-./4-puts
-  
