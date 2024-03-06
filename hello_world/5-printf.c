671047ec9ec4a47bc1c466ddd128e48c97233c3c
Nvhuma <nhlayisekobvuma7@gmail.com>
2024-03-06 06:42:51 +0000

    file

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-
 int main() {
-    // Using the function printf to print the desired message with proper grammar
-    printf("%s, but the outcome is a piece of art,\n", "Programming is like building a multilingual puzzle");
-
-    // End the program with the value 0
-    return 0;
-}
+	printf("%s, but the outcome is a piece of art,\n", "Programming is like building a multilingual puzzle");
+	 return 0;
+	 }
 
