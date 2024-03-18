f2244e973d134009ecad74c81eb6f719098bfc55
Nvhuma <nhlayisekobvuma7@gmail.com>
2024-03-18 06:04:33 +0000

    a

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,15 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char * _memset(char *s, char b, unsinged int n);
-int _putchar(char);
-char * memcpy(char *dest, char *src,  unsinged int n);
-char *_strchr(char *s, char *accept);
-unsinged int _strspn(char *s, char *accept);
-char *_strbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_cheeseboard(char (*a)[8]);
-void print diagsums(int *a, int size);
-void set_string(char **s, char *to);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
+void print_diagsums(int *a, int size);
 
-#endif
+#endif /* MAIN_H */
