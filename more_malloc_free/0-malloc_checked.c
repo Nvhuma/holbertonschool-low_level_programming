4dfa8c6fedc8bcfae461c0cb3449645c23cce6bf
Your Name <you@example.com>
2024-03-25 18:55:55 +0000

    main.h

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,20 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * malloc_checked - cause normal process termination with a status value of 98
- * @b: allocated memory
- * Return: a pointer to the allocated memory
+ * malloc_checked - allocates memory using malloc
+ * @b: the size of the memory to allocate
+ *
+ * Return: pointer to the allocated memory
+ * If malloc fails, the malloc_checked function causes normal process
+ * termination with a status value of 98
  */
 void *malloc_checked(unsigned int b)
 {
-	void *i;
+    void *ptr = malloc(b);
 
-	i = malloc(b);
-	if (i == NULL)
-		exit(98);
-	return (i);
+    if (ptr == NULL)
+        exit(98);
+
+    return ptr;
 }
+
