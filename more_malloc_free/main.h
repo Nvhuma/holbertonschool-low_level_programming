bdfbda6368059e20975883be381b4c31feca0b21
Your Name <you@example.com>
2024-03-25 18:51:21 +0000

    main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,16 +1,10 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * malloc_checked - cause normal process termination with a status value of 98
- * @b: allocated memory
- * Return: a pointer to the allocated memory
- */
-void *malloc_checked(unsigned int b)
-{
-	void *i;
 
-	i = malloc(b);
-	if (i == NULL)
-		exit(98);
-	return (i);
-}
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
